f6b0617650c40002bcc68e522724f79548f41767
Trang Do <dot9716@gmail.com>
2020-04-23 00:26:49 -0400

    extracted first block of bookmarks info

--- browserExtract/browserExtract.cpp
+++ browserExtract/browserExtract.cpp
@@ -6,13 +6,84 @@
 #include <algorithm>
 #include <winsock2.h>
 #include <stdio.h>
+#include <string>
 //#include <Windows.h>
 //#include <cstdio>
 
-
 using namespace std;
 
+ifstream image;
 
+void getBookmarks(int offset)
+{
+    char data;
+    string url = "";
+    string name = "";
+    int i = 0;
+
+    //Finding Timestamp
+    // 0xE0 == length from bookmarks header to start of timestamp
+    int tsOffset = offset + 0xE0;
+    //cout << (hex) << tsOffset << endl;
+    cout << "Timestamp: ";
+    for (i = 0; i < 8; i++)
+    {
+        image.seekg(tsOffset + i);
+        image >> data;
+        cout << (int) data;
+    }
+
+    // Finding Bookamrks counter
+    cout << "\nCounter: ";
+    // counter is 13 bytes from start of timestamp
+    int counterOffset = tsOffset + 13;
+    image.seekg(counterOffset);
+    image >> data;
+    cout << (int)data;
+    
+    
+    // Finding URL 
+    // start of url is 0x210 bytes from start of timestamp
+    int urlOffset = tsOffset + 0x210;    
+    image.seekg(urlOffset);
+    image >> data;
+    i = 0;
+    while (data != NULL && i < 400)
+    {
+        image.seekg(urlOffset + i);
+        image >> data;        
+        url += data;
+        i++;
+    }
+    cout << "\nURL: " << url;
+
+
+    // Finding Bookmarks Name
+    // start of bookmarks name is 0x610 bytes from start of timestamp
+    int nameOffset = tsOffset + 0x610;
+    image.seekg(nameOffset);
+    image >> data;
+    i = 0;
+    while (i < 200)
+    {
+        image.seekg(nameOffset + i);
+        image >> data;
+        name += data;
+        i+=2;
+    }
+    cout << "\nBookmark name: " << name;
+
+
+    /*
+    // timestamp: 28 8 byte chunks from bookmarks header
+    cout << (hex) << data[(pos + 28)] << endl;
+    // Counter: 29 8 byte chunks from bookmarks header
+    cout << (hex) << data[(pos + 29)] << endl;
+    // URL: 55 8 byte chunks from bookmarks header
+    cout << (char) data[(pos + 55)] << endl;
+    */
+    
+}
 
 int main()
 {
@@ -20,7 +91,7 @@ int main()
     short selection = 0;
     int pos = 0;
     string imageLoc;
-    ifstream image;
+    
 
     // getting file location
     //cout << "Please enter the full path for the 3DS file: ";
@@ -41,17 +112,33 @@ int main()
 
         // data gets read in backwards; first hex is now last
         image.read((char*)data, length);
-        image.close();
         
+        
+        // flipping hex values
         for (int i = 0; i < length; i++) 
         {
             data[i] = htonll(data[i]);
         }
-        //data[0] = htonll(data[0]);
-        cout <<  (hex) << data[0] << endl;
+        
+        bool found = FALSE;
+        // searching for the bookmarks header; 0x0100 0000 80DF 0A00
+        while (!found)
+        {
+            if (data[pos] == 0x0100000080DF0A00)
+            {
+                found = TRUE;
+                //cout << pos << endl;
+                // pos * 8 == offset of start of header
+                getBookmarks(pos * 8);
+            }
+            pos++;
+        }
+
         
     }
     image.close();
+    cout << "\n\nFile closed\n\n";
+    
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
